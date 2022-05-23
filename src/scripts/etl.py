220c3ddc16ba2d6e39f9d2a918d8808c76e49571
Paul Melkert <p.g.melkert@gmail.com>
2022-05-23 13:44:21 +0100

    Added docstrings

--- src/scripts/etl.py
+++ src/scripts/etl.py
@@ -4,18 +4,33 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """Load staging data from S3.
+    Args:
+        cur (cursor): psycopg cursor 
+        conn (connetion): Encapsulation of PostgreSQL database session
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """Load table data from staging tables.
+    Args:
+        cur (cursor): psycopg cursor 
+        conn (connetion): Encapsulation of PostgreSQL database session
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Establishes a connection with the data warehouse in the Redshift cluster, 
+    get's the cursor to it and extracts, transforms and loads the song and log data into the staging tables before
+    copying this data into the final tables. Finally, closing the connection.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
